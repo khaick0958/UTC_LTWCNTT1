e4ffbebb4fb3096a77fb5cc62dad15457a066699
khaick0958 <phamquangkhai093@gmail.com>
2026-08-12 21:38:07 +0700

    Add features 9, 10, 11

--- PqkLesson01/StudentService.cs
+++ PqkLesson01/StudentService.cs
@@ -38,13 +38,19 @@ namespace PqkLesson01
             students.Add(student);
         }
 
+        //Format hiển thị 1 sinh viên
+        static void ShowSinhVien(Student student)
+        {
+            Console.WriteLine($"Mã sinh viên: {student.masv}, Họ tên: {student.hoTen}, Ngày sinh: {student.ngaySinh.ToString("dd/MM/yyyy")}, Giới tính: {(student.gioiTinh ? "Nam" : "Nữ")}, Email: {student.email}, Số điện thoại: {student.soDienThoai}, Ngành học: {student.nghanhHoc}, Điểm trung bình: {student.diemTrungBinh}, Trạng thái: {(student.trangThai ? "Đang học" : "Nghỉ học")}");
+        }
+
         //Hiển thị danh sách
         static void HienThiDanhSach(List<Student> students)
         {
             Console.WriteLine("Danh sách sinh viên:");
             foreach (var student in students)
             {
-                Console.WriteLine($"Mã sinh viên: {student.masv}, Họ tên: {student.hoTen}, Ngày sinh: {student.ngaySinh.ToString("dd/MM/yyyy")}, Giới tính: {(student.gioiTinh ? "Nam" : "Nữ")}, Email: {student.email}, Số điện thoại: {student.soDienThoai}, Ngành học: {student.nghanhHoc}, Điểm trung bình: {student.diemTrungBinh}, Trạng thái: {(student.trangThai ? "Đang học" : "Nghỉ học")}});
+                ShowSinhVien(student);
             }
         }
 
@@ -61,13 +67,86 @@ namespace PqkLesson01
             {
                 if (student.masv == masv)
                 {
-                    Console.WriteLine($"Mã sinh viên: {student.masv}, Họ tên: {student.hoTen}, Ngày sinh: {student.ngaySinh.ToString("dd/MM/yyyy")}, Giới tính: {(student.gioiTinh ? "Nam" : "Nữ")}, Email: {student.email}, Số điện thoại: {student.soDienThoai}, Ngành học: {student.nghanhHoc}, Điểm trung bình: {student.diemTrungBinh}, Trạng thái: {(student.trangThai ? "Đang học" : "Nghỉ học")}});
+                    ShowSinhVien(student);
                     trangThai = true;
                     break;
                 }
             }
 
             if (!trangThai) Console.WriteLine("Không tìm thấy kết quả!");
+        }
+
+        //Tìm gần đúng theo họ tên
+        
+        //Cập nhật sinh viên
+
+        //Xóa sinh viên
+
+        //Sắp xếp theo họ tên
+
+        //Sắp xếp theo điểm trung bình
+
+        //Hiển thị sinh viên có điểm từ 8 trở lên
+        static void HienThiSinhVienCoDiemTrungBinhCaoHon8(List<Student> students)
+        {
+            int counter = 0;
+
+            foreach (var student in students)
+            {
+                if (student.diemTrungBinh >= 8)
+                {
+                    ShowSinhVien(student);
+                    counter++;
+                }
+            }
+
+            if (counter == 0) Console.WriteLine("Không tìm thấy kết quả!");
+        }
+
+        //Lấy điểm trung bình cao nhất
+        static float GetHighestDiemTrungBinh(List<Student> students)
+        {
+            float highestDiemTrungBinh = 0;
+
+            foreach (var student in students)
+            {
+                if (highestDiemTrungBinh < student.diemTrungBinh) highestDiemTrungBinh = student.diemTrungBinh;
+            }
+
+            return highestDiemTrungBinh;
+        }
+
+        //Hiển thị sinh viên có điểm cao nhất
+        static void HienThiSinhVienTop1(List<Student> students)
+        {
+            float highestDiemTrungBinh = GetHighestDiemTrungBinh(students);
+
+            foreach (var student in students)
+            {
+                if (student.diemTrungBinh == highestDiemTrungBinh)
+                {
+                    ShowSinhVien(student);
+                }
+            }
+        }
+
+        //Tính điểm trung bình của toàn bộ sinh viên
+        static float AverageDiemTrungBinh(List<Student> students)
+        {
+            float sum = 0;
+            
+            foreach (var student in students)
+            {
+                sum += student.diemTrungBinh;
+            }
+
+            return sum / students.Count;
+        }
+
+        //In ra điểm trung bình toàn bộ sinh viên
+        static void DiemTrungBinhCuaToanBoSinhVien(List<Student> students)
+        {
+            Console.WriteLine($"Điểm trung bình của toàn bộ sinh viên: {AverageDiemTrungBinh(students)}");
         }
     }
 }
